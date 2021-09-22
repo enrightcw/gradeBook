074fd08437cea19644404ba73fb74594a266fcd2
enrightcw <enrightcw@gmail.com>
2021-09-22 10:27:36 -0400

    First commit, some work completed already prior to deciding to add to Git

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var book = new Book("Chris' Grade Book");           
+
+            while ( true ) 
+            {
+                Console.WriteLine($"Enter a grade or type q to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+
+            }
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"For the book named {book.Name}.");
+            Console.WriteLine($"The highest grade is {stats.Low}." );
+            Console.WriteLine($"The lowest grade is {stats.High}." );
+            Console.WriteLine($"The average grade is {stats.Average:N1}." );
+            Console.WriteLine($"The letter is {stats.Letter}." );
+
+        }
+    }
+}
---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,137 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class TypeTests
+    {
+
+        // [Fact]
+        // public void GradeInBounds()
+        // {
+        //     //arrange
+        //     var book = new Book("");
+        //     book.AddGrade(105);
+        //     //act
+            
+
+        //     //assert
+        //     Assert.Equal(, 105);
+                      
+        // }
+
+        [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            GetBookSetName(out book1, "New Name");
+            //act
+            
+
+            //assert
+            Assert.Equal("New Name", book1.Name);
+                      
+        }
+
+        private void GetBookSetName(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CShapIsPassByValue()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+            //act
+            
+
+            //assert
+            Assert.Equal("Book 1", book1.Name);
+                      
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+            book.Name = name;
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+            //act
+            
+
+            //assert
+            Assert.Equal("New Name", book1.Name);
+                      
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+            
+            //act
+            
+
+            //assert
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+                      
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            //arrange
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+            
+            //act
+            
+
+            //assert
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+                      
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
+        }
+    }
+}
