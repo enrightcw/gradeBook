e3e975974b128c89008ec6c4e93cfc62492464dd
enrightcw <enrightcw@gmail.com>
2021-09-28 15:32:47 -0400

    Added GradeAddedDelegate

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    public delegate void GradeAddedDelegate (object sender, EventArgs args); 
+
     public class Book
     {
         public Book(string name)
@@ -40,12 +42,19 @@ namespace GradeBook
             if(grade <= 100  && grade >= 0 )
             {
                 grades.Add(grade);
+
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
             }
             else
             {
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
+
+        public event GradeAddedDelegate GradeAdded;
 
         public Statistics GetStatistics()
         {
@@ -92,8 +101,17 @@ namespace GradeBook
 
         public string Name
         {
-            get; 
-            set;
+             get 
+            {
+                return name;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
         }
 
         public const string CATEGORY = "Science";
