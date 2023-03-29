fcb40a86fb3a5b868a6b76d7870f1e367b5c2036
enrightcw <58670012+enrightcw@users.noreply.github.com>
2023-03-29 13:09:55 -0400

    created a new class to establish Book

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+
             var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
             grades.Add (56.1);
+            grades.
 
             var result = 0.0;
 
