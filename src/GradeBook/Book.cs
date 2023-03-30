5ed87d119e12704d7298106586f90bffac98be8a
enrightcw <58670012+enrightcw@users.noreply.github.com>
2023-03-30 10:25:13 -0400

    built out the ShowStatistics method and moved our math functions into it

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,7 +1,40 @@
+using System;
+using System.Collections.Generic;
 namespace GradeBook
 {
-        class Book
+    class Book
     {
-        
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+
+            foreach(var number in grades)
+            {
+                highGrade = Math.Max (highGrade, number);
+                lowGrade = Math.Min (lowGrade, number);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}.");
+            Console.WriteLine($"The highest grade is {highGrade:N1}.");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}.");
+
+        }
+
+        private List <double> grades;
+        private string name;
     }
 }
